2829a99017306284fcae19f91a1f506d0455dcd3
MasuqaT <occar@hotmail.co.jp>
2026-05-09 11:27:38 +0900

    check point 3

--- LeanGherkin/Syntax.lean
+++ LeanGherkin/Syntax.lean
@@ -6,13 +6,15 @@ namespace LeanGherkin
 open Lean Parser Term PrettyPrinter
 
 def rawTextUntilLineEnd : Parser :=
-{ fn := fun c s =>
-    let startPos := s.pos
-    dbg_trace startPos
-    let s := takeUntilFn (fun c => c == '\n') c s
-    let str := startPos.extract c.inputString s.pos
-    dbg_trace str
-    s.pushSyntax (Syntax.atom SourceInfo.none str)
+{ fn := fun context state =>
+    let startPos := state.pos
+    let newState := takeUntilFn (fun char => char == '\n') context state
+    let str := startPos.extract context.inputString newState.pos
+    
+    let stateAfterNL := newState.next context newState.pos
+    let finalState := whitespace context stateAfterNL
+    
+    finalState.pushSyntax (Syntax.atom SourceInfo.none str)
 }
 
 @[combinator_formatter rawTextUntilLineEnd]
@@ -34,14 +36,9 @@ syntax "But " gherkinText : gherkinStep
 syntax scenarioSyntaxOpening := "Scenario:"
 syntax featureSyntaxOpening := "Feature:"
 
--- TODO FIXME: Feature の次に Scenario が来るとおかしくなる
 -- syntax (name := scenarioSyntax) scenarioSyntaxOpening gherkinText ppLine gherkinStep* : gherkinScenario
 syntax (name := scenarioSyntax) scenarioSyntaxOpening: gherkinScenario
-syntax (name := featureSyntax) featureSyntaxOpening gherkinText ppLine gherkinScenario+ : command
--- syntax (name := featureSyntax) featureSyntaxOpening gherkinText: command -- TODO FIXME: これでもエラーになる
--- error: Test/Diagnostics.lean:13:2: unexpected token 'Scenario:'; expected command なので、 scenario の定義方法か何かがおがしい
--- gherkinScenario 単体や gherkinScenario+ だと先にエラーが出て、gherkinScenario* だと後にエラーが出る。
--- TODO: いっきに parse するのでなく、context や monad に押しやってしまうのが良いかもしれない
+syntax (name := featureSyntax) featureSyntaxOpening gherkinText (colGt gherkinScenario)*: command
 
 syntax (name := stepDefSyntax) "step_def " str (ppSpace funBinder)* " => " term : command
 syntax (name := runFeatureSyntax) "#run_feature " gherkinText : command

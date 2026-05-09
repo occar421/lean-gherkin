0e12906ef35c1ce370f692aebdb11d565269febd
MasuqaT <occar@hotmail.co.jp>
2026-05-09 13:01:07 +0900

    add fixture test

---
+++ Test/WithFixtures.lean
@@ -0,0 +1,32 @@
+import Lean
+import LeanGherkin
+
+open Lean Elab Command
+
+namespace Test.WithFixtures
+
+syntax (name := loadLean) "load_gherkin_files " str : command
+
+@[command_elab loadLean]
+def elabLoadLean : CommandElab := fun stx => do
+  let `(command| load_gherkin_files $pathStx) := stx | throwUnsupportedSyntax
+  let path := pathStx.getString
+  let paths ← liftIO <| do
+    let path := System.FilePath.mk path
+    if ← path.isDir then
+      let entries ← path.readDir
+      pure <| entries.filterMap fun entry =>
+        let p := entry.path
+        if p.extension == some "feature" then some p else none
+    else
+      pure #[path]
+  for path in paths do
+    let content ← liftIO <| IO.FS.readFile path
+    let env ← getEnv
+    match Parser.runParserCategory env `command content with
+    | Except.ok stx => elabCommand stx
+    | Except.error err => throwError (m!"Error in {path}: {err}")
+
+load_gherkin_files "./Test/fixtures"
+
+end Test.WithFixtures
